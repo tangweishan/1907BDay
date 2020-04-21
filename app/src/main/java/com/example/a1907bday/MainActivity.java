d15700d27ec654ebd6703ef562502fb06e87bf7e
tangweishan <61614174+tangweishan@users.noreply.github.com>
2020-04-21 11:22:13 +0800

    Update MainActivity.java
    改3

--- app/src/main/java/com/example/a1907bday/MainActivity.java
+++ app/src/main/java/com/example/a1907bday/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String S ="scbhg";
         String vas="bsdcg";
+        String v="b";
+
+
     }
 }
